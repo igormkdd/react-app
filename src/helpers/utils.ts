87ef115b32ce7b8314d5a1551b64a0eff552b7f0
Igor Janevski <igor_janevski@hotmail.com>
2025-04-07 01:27:16 +0200

    Improve timestamp and add timezone

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,20 +1,14 @@
-export function formatTimestamp(timestamp: string) {
-
+export function formatTimestamp(timestamp: string, timeZone = 'Europe/Skopje') {
     const date = new Date(timestamp);
-
-    // Extract date
-    const year = date.getUTCFullYear();
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-    const day = String(date.getUTCDate()).padStart(2, '0');
-
-    // Extract time
-    const hours = String(date.getUTCHours()).padStart(2, '0');
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0');
-
-    // Format date and time
-    const formattedTime = `${hours}:${minutes}:${seconds}`; // HH:MM:SS
-    const formattedDate = `${day}-${month}-${year}`;  // DD-MM-YYYY
-
-    return `${formattedDate} ${formattedTime}`;
-}
+  
+    return new Intl.DateTimeFormat('en-GB', {
+      timeZone,
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }).format(date);
+  }
+  
